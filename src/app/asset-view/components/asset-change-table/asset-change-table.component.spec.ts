dd1ed08767051eabba6fa56b3681f3ed6a418e69
unknown <luimodotti@gmail.com>
2023-03-12 10:22:06 -0300

    Testes unitários no componente de Tabela

--- src/app/asset-view/components/asset-change-table/asset-change-table.component.spec.ts
+++ src/app/asset-view/components/asset-change-table/asset-change-table.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { AssetChangeTableComponent } from './asset-change-table.component';
 
@@ -8,16 +9,53 @@ describe('AssetChangeTableComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AssetChangeTableComponent ]
-    })
-    .compileComponents();
+      declarations: [AssetChangeTableComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AssetChangeTableComponent);
     component = fixture.componentInstance;
+
+    component.data = [
+      {
+        date: '01/01/2023',
+        price: 10,
+        changeLastDay: null,
+        changeOnPeriod: null,
+      },
+      {
+        date: '02/01/2023',
+        price: 15,
+        changeLastDay: 50,
+        changeOnPeriod: 50,
+      },
+      {
+        date: '03/01/2023',
+        price: 7.5,
+        changeLastDay: -50,
+        changeOnPeriod: -50,
+      },
+    ];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render elements properly', () => {
+    const columns = fixture.debugElement.queryAll(By.css('th'));
+    const rows = fixture.debugElement.queryAll(By.css('tbody > tr'));
+
+    const negative = fixture.debugElement.query(
+      By.css('.negative')
+    ).nativeElement;
+    const positive = fixture.debugElement.query(
+      By.css('.positive')
+    ).nativeElement;
+
+    expect(columns.length).toBe(5);
+    expect(rows.length).toBe(3);
+    expect(parseFloat(negative.innerHTML)).toBeLessThan(0);
+    expect(parseFloat(positive.innerHTML)).toBeGreaterThanOrEqual(0);
   });
 });
