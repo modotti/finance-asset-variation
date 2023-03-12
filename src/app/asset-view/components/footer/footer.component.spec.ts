3922b8a5080f330ad87b5f8753dd8038ac43d0b7
unknown <luimodotti@gmail.com>
2023-03-12 09:58:33 -0300

    Testes unitários nos componentes Header e Footer

--- src/app/asset-view/components/footer/footer.component.spec.ts
+++ src/app/asset-view/components/footer/footer.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { FooterComponent } from './footer.component';
 
@@ -8,9 +9,8 @@ describe('FooterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
-    })
-    .compileComponents();
+      declarations: [FooterComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
@@ -19,5 +19,19 @@ describe('FooterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render elements properly', () => {
+    const credits = fixture.debugElement.query(
+      By.css('p[id="credits"]')
+    ).nativeElement;
+
+    const link = fixture.debugElement.query(
+      By.css('a[href="https://finance.yahoo.com/"]')
+    );
+
+    expect(link).toBeTruthy();
+    expect(credits).toBeTruthy();
+    expect(credits.innerHTML).toContain('Luiz Fernando Modotti');
   });
 });
--- src/app/asset-view/components/header/header.component.spec.ts
+++ src/app/asset-view/components/header/header.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { HeaderComponent } from './header.component';
 
@@ -8,16 +9,59 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
+      declarations: [HeaderComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+
+    component.symbol = 'TST1';
+    component.assetList = [
+      {
+        symbol: 'TST1',
+        description: 'Teste 1',
+      },
+      {
+        symbol: 'TST2',
+        description: 'Teste 2',
+      },
+    ];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render elements properly', () => {
+    const symbol = fixture.debugElement.query(By.css('h1')).nativeElement;
+    const select = fixture.debugElement.query(By.css('select')).nativeElement;
+
+    expect(symbol).toBeTruthy();
+    expect(symbol.innerHTML).toBe('TST1');
+    expect(select).toBeTruthy();
+    expect(select.childElementCount).toBe(2);
+  });
+
+  it('should emit event when change asset in selector', () => {
+    const changeEventSpy = jest
+      .spyOn(component.changeEvent, 'emit')
+      .mockImplementation();
+
+    const select = fixture.debugElement.query(By.css('select')).nativeElement;
+    select.dispatchEvent(new Event('change'));
+
+    expect(changeEventSpy).toHaveBeenCalled();
+  });
+
+  it('should show symbol of asset in h1 when change asset in selector', () => {
+    const symbol = fixture.debugElement.query(By.css('h1')).nativeElement;
+    const select = fixture.debugElement.query(By.css('select')).nativeElement;
+    select.value = 'TST2';
+    select.dispatchEvent(new Event('change'));
+
+    fixture.detectChanges();
+
+    expect(symbol.innerHTML).toBe('TST2');
   });
 });
